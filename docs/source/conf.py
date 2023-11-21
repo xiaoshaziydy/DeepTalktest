d9086bb21317547c574f7c864980881696e21547
xiaoshaziydy <71171495+xiaoshaziydy@users.noreply.github.com>
2023-11-21 16:22:28 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,9 @@ release = '0.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
+import sys
+import os
+import sphinx
 
 extensions = [
         'recommonmark',
